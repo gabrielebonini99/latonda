7b6204bc5e093dc895b4e00f8a4ba87bb6ba1b88
gabrielebonini99 <114037656+gabrielebonini99@users.noreply.github.com>
2022-11-24 14:49:50 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -16,22 +16,6 @@ function main() {
         }
       });
 
-	
-    // Show Menu on Book
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 600;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    });
-
 	// Hide nav on click
   $(".navbar-nav li a").click(function (event) {
     // check if window is small enough so dropdown is created
@@ -42,7 +26,6 @@ function main() {
   });
 
 }());
-
 
 }
 main();
