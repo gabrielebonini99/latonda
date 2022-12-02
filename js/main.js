2c81279a2dfb110a2f2276126ed50900c0dba000
gabrielebonini99 <114037656+gabrielebonini99@users.noreply.github.com>
2022-12-02 17:16:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,19 +3,6 @@ function main() {
 (function () {
    'use strict';
 
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
-
 	// Hide nav on click
   $(".navbar-nav li a").click(function (event) {
     // check if window is small enough so dropdown is created
