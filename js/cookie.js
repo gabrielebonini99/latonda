2805c710669a7d96b74da10837dd7856cf2d0fce
Gabriele Bonini <114037656+gabrielebonini99@users.noreply.github.com>
2023-07-04 17:39:23 +0200

    Delete cookie.js

--- js/cookie.js
+++
@@ -1,4 +0,0 @@
-cookieValue = document.cookie.match(/(;)?cookiebar=([^;]*);?/)[2];
-if (cookieValue == 'CookieAllowed') {
-    (function(f,n) { n = document.createElement('script'); n.src = 'https://fcdn.answerly.io/fn.js'; n.setAttribute('data-companyId', f); document.getElementsByTagName('html')[0].insertAdjacentElement('beforeend', n); })('9997e9cf-f871-4b9c-b56a-eae356d319cb');
-}
